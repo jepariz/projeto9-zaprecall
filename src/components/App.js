87f375544235c3599ccc36db0b38f9e1ff874147
jepariz <je.parizotto@gmail.com>
2022-09-30 23:27:02 -0300

    Criei a lógica de colorir a pergunta

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,49 @@
-import logo from "../img/logo.png"
-import styled from "styled-components"
-import FlashCards from "./Flashcards"
-import GlobalStyle from "../img/GlobalStyle"
-import Footer from "./Footer"
+import logo from "../img/logo.png";
+import styled from "styled-components";
+import FlashCards from "./Flashcards";
+import GlobalStyle from "../GlobalStyle";
 
 
+export default function App() {
+  
 
-export default function App (){
-    return (
-       <Body>
-        <GlobalStyle/>
-        <ContainerLogo>
-        <img src={logo}/>
+
+  return (
+    <Body>
+      <GlobalStyle />
+      <ContainerLogo>
+        <img src={logo} />
         <h1>ZapRecall</h1>
-        </ContainerLogo>
-        <FlashCards/>
-        <Footer/>
-       </Body>
-    )
+      </ContainerLogo>
+      <FlashCards/>
+    </Body>
+  );
 }
-
-
 
 const Body = styled.div`
-width: 100%;
-display: flex;
-flex-direction: column;
-align-items: center;
-`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 const ContainerLogo = styled.div`
-margin-top: 40px;
-width: 255px;
-height: 60px;
-display: flex;
-gap: 20px;
-align-items: center;
+  margin-top: 40px;
+  width: 255px;
+  height: 60px;
+  display: flex;
+  gap: 20px;
+  align-items: center;
 
-img{
+  img {
     width: 52px;
     height: 60px;
-}
+  }
 
-h1{
+  h1 {
     font-size: 36px;
     color: white;
-    font-family: 'Righteous', cursive;
+    font-family: "Righteous", cursive;
     font-weight: 400;
-}
-`
+  }
+`;
+
--- src/components/Flashcards.js
+++ src/components/Flashcards.js
@@ -3,50 +3,119 @@ import styled from "styled-components";
 import play from "../img/play.svg";
 import setinha from "../img/setinha.png";
 import perguntasRespostas from "./arrayPerguntasRespostas";
+import check from "../img/check.png";
+import wrong from "../img/wrong.png";
+import question from "../img/question.png"
+
 
 export default function FlashCards() {
+  const [naoLembrei, setNaoLembrei] = useState([]);
+  const [quaseLembrei, setQuaseLembrei] = useState([]);
+  const [zap, setZap] = useState([]);
+  const [respostaExibida, setRespostaExibida] = useState([]);
   const [flashCardClicado, setFlashCardClicado] = useState([]);
+  const [icone, setIcone] = useState("setinha")
   const [mostrarResposta, setMostrarResposta] = useState(false);
 
   function mostrarPergunta(index) {
     if (!flashCardClicado.includes(index)) {
       setMostrarResposta(false);
-      const novoArray = [index];
-      setFlashCardClicado(novoArray);
+      const novoArray = index;
+      setFlashCardClicado([novoArray]);
     }
   }
 
+  function exibirResposta(index) {
+    setMostrarResposta(true);
+    if (!respostaExibida.includes(index)) {
+      const resposta = [...respostaExibida, index];
+      setRespostaExibida(resposta);
+    }
+  }
+
+  function trocarCor1(index) {
+    const esquecido = [...naoLembrei, index];
+    setNaoLembrei(esquecido);
+    setIcone("wrong")
+  }
+
+  function trocarCor2(index) {
+    const quase = [...quaseLembrei, index];
+    setQuaseLembrei(quase);
+    setIcone("question") 
+  }
+
+  function trocarCor3(index) {
+    const lembrei = [...zap, index];
+    setZap(lembrei);
+    setIcone("check") 
+  }
+
   return (
-    <ContainerPerguntas>
-      {perguntasRespostas.map((p, index) => (
-        <Perguntas
-          key={index}
-          onClick={() => mostrarPergunta(index)}
-          flashCardClicado={flashCardClicado.includes(index)}
-        >
-          {flashCardClicado.includes(index) ? (
-            mostrarResposta ? (
-              <p>{p.resposta}</p>
+    <>
+      <ContainerPerguntas>
+        {perguntasRespostas.map((p, index) => (
+          <Perguntas
+            key={index}
+            onClick={() => mostrarPergunta(index)}
+            flashCardClicado={flashCardClicado.includes(index)}
+            respostaExibida={respostaExibida.includes(index)}
+            naoLembrei={naoLembrei.includes(index)}
+            quaseLembrei={quaseLembrei.includes(index)}
+            zap={zap.includes(index)}
+          >
+            {flashCardClicado.includes(index) ? (
+              mostrarResposta ? (
+                <Respostas>
+                  <p>{p.resposta}</p>
+                  <div>
+                    <button onClick={() => trocarCor1(index)}>
+                      Não Lembrei
+                    </button>
+                    <button onClick={() => trocarCor2(index)}>
+                      Quase Lembrei
+                    </button>
+                    <button onClick={() => trocarCor3(index)}>Zap!</button>
+                  </div>
+                </Respostas>
+              ) : (
+                <p>{p.pergunta}</p>
+              )
             ) : (
-              <p>{p.pergunta}</p>
-            )
-          ) : (
-            <p>Pergunta {index + 1}</p>
-          )}
-          {flashCardClicado.includes(index) ? (
-            mostrarResposta ? (
-              ""
+              <p>Pergunta {index + 1}</p>
+            )}
+            {flashCardClicado.includes(index) ? (
+              mostrarResposta ? (
+                ""
+              ) : (
+                <img src={setinha} onClick={() => exibirResposta(index)}></img>
+              )
             ) : (
-              <img src={setinha} onClick={() => setMostrarResposta(true)}></img>
-            )
-          ) : (
-            <img src={play}></img>
-          )}
-        </Perguntas>
-      ))}
-    </ContainerPerguntas>
+              <img src={(respostaExibida.includes(index) ? naoLembrei.includes(index) ? wrong : quaseLembrei.includes(index) ? question : zap.includes(index) ? check : play : play)}></img>
+            )}
+          </Perguntas>
+        ))}
+      </ContainerPerguntas>
+      <Rodape>
+        <span>4/4 CONCLUÍDOS</span>
+        <div>
+          <img src={check} />
+          <img src={wrong} />
+        </div>
+      </Rodape>
+    </>
   );
 }
+
+const ContainerPerguntas = styled.ul`
+  margin-top: 50px;
+  width: 90%;
+  height: auto;
+  display: grid;
+  justify-content: center;
+  gap: 20px;
+  margin-bottom: 180px;
+`;
 
 const Perguntas = styled.li`
   width: 300px;
@@ -65,7 +134,27 @@ const Perguntas = styled.li`
     font-family: "Recursive", sans-serif;
     font-weight: ${(prop) => (prop.flashCardClicado ? "400" : "700")};
     font-size: 16px;
-    color: #333333;
+    color: ${(prop) =>
+      prop.respostaExibida
+        ? prop.naoLembrei
+          ? "#FF3030"
+          : prop.quaseLembrei
+          ? "#FF922E"
+          : prop.zap
+          ? "#2FBE34"
+          : "#333333"
+        : "#333333"};
+
+    text-decoration: ${(prop) =>
+      prop.respostaExibida
+        ? prop.naoLembrei
+          ? "line-through"
+          : prop.quaseLembrei
+          ? "line-through"
+          : prop.zap
+          ? "line-through"
+          : null
+        : null};
   }
 
   img {
@@ -77,12 +166,79 @@ const Perguntas = styled.li`
   }
 `;
 
-const ContainerPerguntas = styled.ul`
-  margin-top: 50px;
-  width: 90%;
-  height: auto;
-  display: grid;
+const Respostas = styled.div`
+  display: flex;
+  flex-direction: column;
   justify-content: center;
-  gap: 20px;
-  margin-bottom: 180px;
+  align-items: center;
+
+  p {
+    font-family: "Recursive", sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    color: ${(prop) =>
+      prop.respostaExibida
+        ? prop.naoLembrei
+          ? "#FF3030"
+          : prop.quaseLembrei
+          ? "orange"
+          : prop.zap
+          ? "green"
+          : "#333333"
+        : "#333333"};
+    margin-bottom: 20px;
+  }
+
+  div {
+    display: flex;
+    gap: 10px;
+
+    button {
+      width: 85px;
+      height: 37px;
+      border-radius: 5px;
+      border: none;
+      color: #fff;
+      font-size: 12px;
+      text-align: center;
+      font-family: "Recursive", sans-serif;
+      &:nth-child(1) {
+        background-color: #ff3030;
+      }
+
+      &:nth-child(2) {
+        background-color: #ff922e;
+      }
+
+      &:nth-child(3) {
+        background-color: #2fbe34;
+      }
+    }
+  }
+`;
+
+const Rodape = styled.div`
+  width: 100%;
+  height: 111px;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  z-index: 1;
+
+  span {
+    font-family: "Recursive", sans-serif;
+    font-size: 18px;
+    color: #333333;
+  }
+
+  div {
+    display: flex;
+    gap: 5px;
+    margin-top: 10px;
+  }
 `;
